7130e3784100afe939593f411e14ffa0de605e12
Bruno Kauã <brunokauaalvesdecarvalho@gmail.com>
2023-05-16 20:23:43 -0300

    Corrigindo main  tsx para validar na plataforma

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './paginas/home/Home';
-import Navbar from './components/estaticos/navbar/Navbar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './paginas/login/login'
-import Footer from './components/estaticos/footer/Footer';
-
-
-
-
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-     <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route  path="/login" element={<Login />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root') as HTMLElement
 );
