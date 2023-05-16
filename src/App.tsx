be053fb337fa46849cbdd68a540e33b223cb657f
Bruno Kauã <brunokauaalvesdecarvalho@gmail.com>
2023-05-16 20:15:42 -0300

    Corrigindo appp tsx para validar na plataforma

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import Navbar from './components/estaticos/navbar/Navbar'
-import Footer from './components/estaticos/footer/Footer'
-import Home from './paginas/home/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './paginas/login/login'
+import Footer from './components/estaticos/footer/Footer'
+import Navbar from './components/estaticos/navbar/Navbar'
+import Home from './paginas/home/Home'
+import Login from './paginas/login/Login'
 
 function App() {
 
   return (
     <>
-      nao usar 
+      <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />}/>
+          </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
-
   )
 }
 
