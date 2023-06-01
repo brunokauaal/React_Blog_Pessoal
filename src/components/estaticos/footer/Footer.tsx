a65347e919620949e8f1e62e2f267459767e7867
Bruno Kauã <brunokauaalvesdecarvalho@gmail.com>
2023-06-01 15:04:00 -0300

    Atualizando icones footer

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,10 +1,10 @@
 import { Grid, Typography } from '@material-ui/core';
-import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import { Box } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { UserState } from '../../../store/token/Reducer';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function Footer() {
 
@@ -23,13 +23,13 @@ function Footer() {
                             <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noopener noreferrer">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+                            <a href="https://github.com/brunokauaal" target="_blank" rel="noopener noreferrer">
+                                <GitHubIcon style={{ fontSize: 60, color: "white" }} />
                             </a>
-                            <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noopener noreferrer">
+                            <a href="https://www.instagram.com/bkauaalves/" target="_blank" rel="noopener noreferrer">
                                 <InstagramIcon style={{ fontSize: 60, color: "white" }} />
                             </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noopener noreferrer">
+                            <a href="https://www.linkedin.com/in/bruno-kaua-655a6922a/" target="_blank" rel="noopener noreferrer">
                                 <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
                             </a>
                         </Box>
