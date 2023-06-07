baf735815b5a11bff35799fe33d76d6cd9002e6a
Bruno Kauã <brunokauaalvesdecarvalho@gmail.com>
2023-06-07 16:39:19 -0300

    Corrigindo label do cadastro e trocando imagem do caadastro

--- src/paginas/cadastroUsuario/CadastroUsuario.tsx
+++ src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -112,7 +112,7 @@ function CadastroUsuario() {
 
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
-            <Grid item xs={6} className='imagem2'></Grid>
+            <Grid item xs={6} className='imagem3'></Grid>
             <Grid item xs={6} alignItems='center'>
                 <Box paddingX={10}>
                     <form onSubmit={onSubmit}>
@@ -166,7 +166,7 @@ function CadastroUsuario() {
                             value={confirmarSenha}
                             onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)}
                             id='confirmarSenha'
-                            label='confirmarSenha'
+                            label='confirmar Senha'
                             variant='outlined'
                             name='confirmarSenha'
                             margin='normal'
